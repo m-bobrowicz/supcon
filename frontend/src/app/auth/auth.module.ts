6fde44d29a3ee1e4ed6c982cc49093190dd5f405
atmxtn <oskarwojcik@proton.me>
2023-03-07 01:38:27 +0100

    ##9 user profile change password

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { RouterModule } from '@angular/router';
 import { SignInComponent } from 'src/app/auth/sign-in/sign-in.component';
 import { SignInModule } from 'src/app/auth/sign-in/sign-in.module';
 
+
 @NgModule({
   imports: [
-    RouterModule.forChild([{ path: 'sign-in', component: SignInComponent }]),
+    RouterModule.forChild([{ path: 'sign-in', component: SignInComponent}]),
     SignInModule,
   ],
   exports: [],
---
+++ frontend/src/app/auth/change-password/change-password.component.ts
@@ -0,0 +1,73 @@
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, switchMap} from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'sc-auth-change-password',
+  templateUrl: './change-password.component.html',
+})
+export class ChangePasswordComponent {
+  formGroup = this.fb.group({
+    currentPassword: ['', Validators.required],
+    newPassword: ['', Validators.required],
+    repeatPassword: ['', Validators.required]
+  });
+
+
+  submit() {
+    this.resetForm();
+    this.formGroup.markAllAsTouched();
+
+    if (this.formGroup.invalid) {
+      return;
+    }
+
+
+    const value = this.formGroup.value as {
+      currentPassword: string;
+      newPassword: string;
+      repeatPassword: string;
+    };
+
+
+    
+    this.auth.whoAmI().pipe(
+      switchMap(user => {
+          return this.auth.changePassword({...value, username: user.username}).pipe(
+            catchError((error) => {
+              if (
+                error instanceof HttpErrorResponse &&
+                error.status === HttpStatusCode.Unauthorized
+              ) {
+                this.formGroup.setErrors({ wrongCredentials: true });
+              }
+              throw error;
+            })
+          )
+        }
+            
+      )
+    ).subscribe(() => this.router.navigateByUrl('app/user-profile'));
+
+
+  
+  
+    }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.formGroup.setErrors(null);
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
+}
