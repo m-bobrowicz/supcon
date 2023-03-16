c8a88a94da6d94870f141fbb58cffa18e461ae77
Michal Bobrowicz <michal.bobrowicz@exorigo-upos.pl>
2023-03-17 00:09:20 +0100

    closes #29 multiple config locations

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,34 +1,33 @@
-import { readFileSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { resolve } from 'path';
 
-const YAML_CONFIG_FILENAME = 'config/config.yml';
+const YAML_CONFIG_FILENAMES = ['config.yml', 'config/config.yml'];
 
 export const loadConfiguration = () => {
-  const path = resolve(YAML_CONFIG_FILENAME);
-  try {
-    const data = yaml.load(readFileSync(path, 'utf8')) as Record<string, any>;
-    return {
-      isDev: data.isDev,
-      http: {
-        host: data.http.host,
-        port: data.http.port,
-        jwtSecret: data.http.jwtSecret,
-      },
-      database: {
-        host: data.db.host,
-        port: data.db.port,
-        user: data.db.user,
-        password: data.db.password,
-        dbName: data.db.dbName,
-      },
-    };
-  } catch (error) {
-    if ((error as { code: string })?.code === 'ENOENT') {
-      throw new Error(
-        `FILE NOT FOUND: ${path} was not found. If you're setting up dev environment: copy config/config.dev.yml file as config/config.yml`,
-      );
-    }
-    throw error;
+  const location = YAML_CONFIG_FILENAMES.find((it) => existsSync(resolve(it)));
+
+  if (location == null) {
+    throw new Error(
+      `FILE NOT FOUND: config was not found. If you're setting up dev environment: copy config/config.dev.yml file as config/config.yml`,
+    );
   }
+
+  const path = resolve(location);
+  const data = yaml.load(readFileSync(path, 'utf8')) as Record<string, any>;
+  return {
+    isDev: data.isDev,
+    http: {
+      host: data.http.host,
+      port: data.http.port,
+      jwtSecret: data.http.jwtSecret,
+    },
+    database: {
+      host: data.db.host,
+      port: data.db.port,
+      user: data.db.user,
+      password: data.db.password,
+      dbName: data.db.dbName,
+    },
+  };
 };
