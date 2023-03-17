c2d40ab98c266bce3ca474494f08d3141c3a96af
Michal Bobrowicz <michal.bobrowicz@exorigo-upos.pl>
2023-03-17 23:33:25 +0100

    closes #35 who am i loading indicator

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map } from 'rxjs';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  isNotLoading$ = this.appService.isLoading$.pipe(
+    map((isLoading) => isLoading === false)
+  );
+
+  constructor(private appService: AppService) {}
+}
