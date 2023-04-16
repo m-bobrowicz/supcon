1aefea77f1f8b4decf553e2dad364e4eda1f361b
atmxtn <oskarwojcik@proton.me>
2023-04-16 13:20:05 +0200

    closes #51 new user profile actions

--- frontend/src/app/lib/menu/menu.directive.ts
+++ frontend/src/app/lib/menu/menu.directive.ts
@@ -4,5 +4,5 @@ import { Directive, HostBinding } from '@angular/core';
 export class MenuDirective {
   @HostBinding('class')
   readonly elementClass =
-    'flex flex-col inline-flex mt-2';
+    'flex flex-col inline-flex overflow-hidden rounded-lg mt-2 shadow-md shadow-zinc-800 bg-zinc-700 ';
 }
