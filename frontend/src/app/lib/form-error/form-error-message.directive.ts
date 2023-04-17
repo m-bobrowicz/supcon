24136f8d3ffc8b329c44773ae87ac469fe63d6af
Michal Bobrowicz <michal.bobrowicz@exorigo-upos.pl>
2023-04-17 23:44:14 +0200

    WIP

--- frontend/src/app/lib/form-error/form-error-message.directive.ts
+++ frontend/src/app/lib/form-error/form-error-message.directive.ts
@@ -1,23 +1,46 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Directive,
+  HostBinding,
+  Input,
+  SkipSelf,
+} from '@angular/core';
 import { AbstractControl } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Directive({ selector: '[scFormErrorMessage]' })
 export class FormErrorDirective {
   @HostBinding('class.hidden')
   get isHidden() {
-    if (this.checkFor == null) {
-      return false === (this.control?.touched && this.control?.invalid);
-    }
-    return (
-      false === this.control?.touched &&
-      false === this.control?.hasError(this.checkFor)
-    );
+    const isVisible = (() => {
+      if (this.checkFor == null) {
+        return this.control?.touched && this.control?.invalid;
+      }
+      return this.control?.touched && this.control?.hasError(this.checkFor);
+    })();
+    return !isVisible;
   }
 
   @HostBinding('class.text-red-500')
   textClass = true;
 
-  @Input('scFormErrorMessage') control: AbstractControl | null = null;
+  _control: AbstractControl | null = null;
+  get control() {
+    return this._control;
+  }
+  @Input('scFormErrorMessage') set control(_control: AbstractControl | null) {
+    this._control = _control;
+    this._control?.statusChanges
+      .pipe(
+        tap(() => {
+          this.cdr.detectChanges();
+          this.cdr.markForCheck();
+        })
+      )
+      .subscribe();
+  }
 
   @Input('scFormErrorMessageCheckFor') checkFor: string | null = null;
+
+  constructor(@SkipSelf() private cdr: ChangeDetectorRef) {}
 }
